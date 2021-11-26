eac925048ccdea2670ec5d6c9c2f588ae9f96c31
KKlnojl <kkbar03@gmail.com>
2021-11-26 09:11:17 +0800

    2. Second Version: Solve to do list, Complete

--- main.py
+++ main.py
@@ -85,13 +85,17 @@ class Ui:
     def select_img(self):
         path = dialog.askopenfilename(title="選擇檔案(Choose files)", filetypes=(("JPG", ".jpg"), ("PNG", ".png")))
         self.img = Image.open(path).convert("RGBA")  # 以rgba方式讀取檔案，以利後與含浮水印圖片結合
+        self.img_width = self.img.size[0]  # 將原始圖片大小儲存，瀏覽後最後要再轉回原本大小用
+        self.img_height = self.img.size[1]  # 將原始圖片大小儲存，瀏覽後最後要再轉回原本大小用
         self.show()
 
     def show(self):
+        self.img = self.img.resize((self.img_width, self.img_height))  # 將圖還原(初始化)，以利重新計算比例縮放顯示
         self.img_width = self.img.size[0]  # 將原始圖片大小儲存，瀏覽後最後要再轉回原本大小用
         self.img_height = self.img.size[1]  # 將原始圖片大小儲存，瀏覽後最後要再轉回原本大小用
         self.resize(w=self.img_width, h=self.img_height, w_box=self.field_img.winfo_width(),
                     h_box=self.field_img.winfo_height())
+        print(f"show add rotate x: {self.img_width} rotate y: {self.img_height}")
         self.show_img = ImageTk.PhotoImage(self.show_img, self.show_img.size)  # 用ImageTk模組來讀取圖片，讓其可以tkinter內使用
         self.canvas_img = self.canvas.create_image(self.px, self.py, image=self.show_img)  # 將圖片放到canvas中心點。
 
@@ -107,7 +111,6 @@ class Ui:
         y = self.img_width
         self.img_width = x
         self.img_height = y
-        print(f"rotate x: {self.img_width} rotate y: {self.img_height}")
 
     def get_center(self):
         self.canvas.update()
@@ -153,11 +156,9 @@ class Ui:
         self.img = Image.alpha_composite(self.img, fake_img)
         self.show_img = ImageTk.PhotoImage(image=self.img, size=self.img)
         self.canvas.itemconfig(self.canvas_img, image=self.show_img)
-        self.img = self.img.resize((self.img_width, self.img_height))  # 還原原始圖片大小解析度
+        self.img = self.img.resize((self.img_width, self.img_height))  # 預覽圖片
         self.img.show()
-        print(f"add rotate x: {self.img_width} rotate y: {self.img_height}")
 
-# todo:轉換有問題，圖沒有還原
     def get_wm_position(self):
         # 預設置中
         self.wm_x = self.img.size[0] // 2
@@ -168,6 +169,7 @@ class Ui:
         elif self.option_var.get() == 2:
             self.wm_x = self.img.size[0] // 2
             self.wm_y = self.img.size[1]
+        # print(f"get wm position  rotate x: {self.img_width} rotate y: {self.img_height}")
 
     def save_img(self):
         filename = dialog.asksaveasfile(mode="w", title="Save To", defaultextension=".png",
